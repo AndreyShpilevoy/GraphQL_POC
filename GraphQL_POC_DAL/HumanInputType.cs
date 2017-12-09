e1e08790426902a27e7ab8daf32c5fc0a3c2f83c
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-12-09 21:01:12 +0200

    initial commit

---
+++ GraphQL_POC_DAL/HumanInputType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL_DAL
+{
+    public class HumanInputType : InputObjectGraphType
+    {
+        public HumanInputType()
+        {
+            Name = "HumanInput";
+            Field<NonNullGraphType<StringGraphType>>("name");
+            Field<StringGraphType>("homePlanet");
+        }
+    }
+}
---
+++ GraphQL_POC_DAL/StarWarsSchema.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL;
+using GraphQL.Types;
+
+namespace GraphQL_DAL
+{
+    public class StarWarsSchema : Schema
+    {
+        public StarWarsSchema(IDependencyResolver resolver)
+            : base(resolver)
+        {
+            Query = resolver.Resolve<StarWarsQuery>();
+            Mutation = resolver.Resolve<StarWarsMutation>();
+        }
+    }
+}
